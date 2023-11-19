62a3f9326ee1c08a1ce642f2c5f9c4e5991fae85
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-11-19 10:21:32 -0800

    added iterator

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,9 +1,11 @@
 # write your bubble sort algorithm below
 def bubble_sort(lst):
       for i in range(len(lst)):
+          iterator = 0
           swapped = False
-          print(f"iteration")
+          print(f"iteration{iterator}")
           for j in range(len(lst) - 1):
+              iterator += 1
               print(f"comparing{lst[j], lst[j+1]}")
               if lst[j] > lst[j+1]:
                   lst[j], lst[j+1] = lst[j+1], lst[j]
